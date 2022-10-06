b4776435e184b60b0e419754ab90e63b2fdfc307
agiratech-akesavan <kesavan.a@agiratech.com>
2022-10-06 19:27:47 +0530

    templete form validation

--- reactiveform/src/app/login/login.component.ts
+++ reactiveform/src/app/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
 
   output(value:any){
     console.log(value)
-    alert("The Login successfully completed")
+    // alert("The Login successfully completed")
   }
 
   website(){
